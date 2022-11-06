5bd5daa114a144c93de3ff806bc78d3025081818
Zechee <yinzeqi11@gmail.com>
2022-11-06 15:20:29 +1300

    changed states management

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { createStore } from 'vuex'
+import { createStore, storeKey } from 'vuex'
 
 export default createStore({
+  // where we define the state
   state: {
+    name: "Jack"
   },
-  getters: {
-  },
+  // where we can modify the state synchronously
   mutations: {
+    // we need two parameters 1: state, 2: payload (value that we want to change)
+    setName(state, payload) {
+      state.name = payload
+    }
   },
+  //where we submit the mutations asynchronously
   actions: {
+    // needs two parameters 1: store, 2: params
+    asyncSetName(store, params) {
+      setTimeout(() => {
+        store.commit('setName', params)
+      }, 2000)
+    }
   },
+  // moudularization
   modules: {
   }
 })
