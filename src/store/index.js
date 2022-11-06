7df7f321fda7f6cb8511f40c28e1634959c6dcca
Zechee <yinzeqi11@gmail.com>
2022-11-06 15:53:33 +1300

    computed property domo

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,43 @@
-import { createStore, storeKey } from 'vuex'
+import { createStore} from 'vuex'
 
 export default createStore({
   // where we define the state
   state: {
-    name: "Jack"
+    list: [
+      {
+          title: 'Eating',
+          complete: false
+      },
+      {
+          title: 'Sleeping',
+          complete: false
+      },
+      {
+          title: 'Coding',
+          complete: true
+      }
+  ]
   },
   // where we can modify the state synchronously
   mutations: {
     // we need two parameters 1: state, 2: payload (value that we want to change)
-    setName(state, payload) {
-      state.name = payload
+    // adding todo list
+    addTodo(state, payload) {
+      state.list.push(payload)
+    },
+    // delete the task splice(subscript, number)
+    delTodo(state, payload) {
+      state.list.splice(payload, 1)
+    },
+    // celear completed task
+    clear(state, payload) {
+      state.list = payload
     }
+
   },
   //where we submit the mutations asynchronously
   actions: {
     // needs two parameters 1: store, 2: params
-    asyncSetName(store, params) {
-      setTimeout(() => {
-        store.commit('setName', params)
-      }, 2000)
-    }
   },
   // moudularization
   modules: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,18 @@
     <nav-main></nav-main>
     <nav-footer></nav-footer>
   </div>
+  <div>
+    {{list}}
+  </div>
+  <!-- <div>
+    {{num1}} --- {{num2}}
+    The sum of Num1 and Num2 : {{addNum}}
+  </div>
+  <div>
+    <button @click="add">Add</button>
+  </div>  -->
+
+
 </template>
 
 <script>
@@ -12,7 +24,8 @@ import NavHeader from '@/components/navHeader/NavHeader'
 import NavMain from '@/components/navMain/NavMain'
 import NavFooter from '@/components/navFooter/NavFooter'
 
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import {useStore} from 'vuex'
 
 export default defineComponent({
   name: 'Home',
@@ -29,8 +42,28 @@ export default defineComponent({
   },
 
   setup() {
+    let store = useStore()
+    // console.log(store)
+    let list = computed(() => {
+      return store.state.list
+    })
+    // let num1 = ref(10)
+    // let num2 = ref(20)
+    // let addNum = computed(() => {
+    //   return num1.value + num2.value
+    // })
+    // let add = () => {
+    //   num1.value ++
+    //   num2.value ++
+    // }
+
 
     return {
+      // num1,
+      // num2,
+      // addNum,
+      // add
+      list
     }
 
   }
